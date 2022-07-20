46537e5676c5baa8488fa6b43760e27291d4e75e
Whit Morgan <webdevwhit@gmail.com>
2022-07-19 22:21:44 -0400

    updated calculator functin in js. results now displayed in the dom

--- public/js/main.js
+++ public/js/main.js
@@ -1,26 +1,38 @@
-
-//test that the file is properly linked to the index.html
 document.querySelector('#calculateCartridges').addEventListener('click', calculate)
 
-
-
-// let myMaterials = {
-//     bulletAmount : document.querySelector('#bulletAmt').value,
-
-// }
 function calculate(){
+    const casingsPerPound = {
+        '9mm': 117,
+        '40sw': 100
+    }
+
+    //values pulled from the html form set into variables
     let bulletAmount = Number(document.querySelector('#bulletAmt').value)
     let primerAmount = Number(document.querySelector('#primerAmt').value)
-    let casingAmount = Number(document.querySelector('#casingAmt')).value
-    let powderAmout = Number(document.querySelector('#powderAmt')).value
-    let powderUsed = Number(document.querySelector('#powderUsed')).value
+    let casingAmount = Number(document.querySelector('#casingAmt').value)
+    let powderAmount = Number(document.querySelector('#powderAmt').value) * 7000
+    let powderUsed = Number(document.querySelector('#powderUsed').value)
     let chooseCase = document.querySelector('#chooseCase').value
-    console.log(chooseCase)
+
+    //calculated variables for powder and casings
+    let totalPowder = powderAmount / powderUsed
+    let totalCasings = Math.round(casingsPerPound[chooseCase] * casingAmount)
+
+    //put all the elements in an array and sorted to see the lowest number. the lowest number is the max number of loads that can be made
+    let materials = [bulletAmount, primerAmount, totalPowder, totalCasings]
+    
+    console.log(materials)
+
+    //display the max number of makeable cartridges in the dom
+    document.querySelector('#finalNum').innerText = materials.sort((a, b) => a - b)[0]
 }
 
-function changeColor(){
-    document.querySelector('body').style.background = 'blue'
-}
+
+
+
+
+
+/////MISC NOTES//////
 //ideas on how to organize the fields
 // have it broken out into sections like 'my materials' that could be an object with the prop/values being the input values
 //another section could be cartridge-specific details. these would be the amount of powder used (grains) and the case choice from the dropdown
